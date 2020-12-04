d18ab4e7e387e9481de5fa6cfb754c85b9f8b49a
Corniel Nobel <mail@corniel.nl>
2020-12-04 14:14:17 +0100

    Clean-up.

--- src/AdventOfCode/2019/Intcoding/Mode.cs
+++ src/AdventOfCode/2019/Intcoding/Mode.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode._2019.Intcoding
+﻿namespace AdventOfCode._2019.Intcoding
 {
     public enum Mode
     {
         Position = 0,
-        Immediate,
+        Immediate = 1,
     }
 }

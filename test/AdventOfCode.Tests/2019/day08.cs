a1459e50b53f43c45360ef7ea88351703b7ac216
Corniel Nobel <mail@corniel.nl>
2020-12-04 16:59:04 +0100

    2019-08

---
+++ test/AdventOfCode.Tests/2019/day08.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode._2019;
+using AdventOfCode.Tests;
+using NUnit.Framework;
+
+namespace Advent_of_Code_2019
+{
+    public class day08
+    {
+        [Test]
+        public void part_one()
+        {
+            var input = Input.For(2019, 08);
+            Puzzle.HasAnswer(2480, Day08.One, with: input);
+        }
+  
+        [Test]
+        /// <remarks>
+        /// ZYBLH
+        /// </remarks>
+        public void part_two()
+        {
+            var input = Input.For(2019, 08);
+            Puzzle.HasAnswer(@"
+████░█░░░████░░█░░░░█░░█░
+░░░█░█░░░██░░█░█░░░░█░░█░
+░░█░░░█░█░███░░█░░░░████░
+░█░░░░░█░░█░░█░█░░░░█░░█░
+█░░░░░░█░░█░░█░█░░░░█░░█░
+████░░░█░░███░░████░█░░█░
+", Day08.Two, with: input);
+        }
+    }
+}

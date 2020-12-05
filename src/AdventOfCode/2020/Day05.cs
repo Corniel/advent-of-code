77190ad8c3e95c694ac445071b5222f1fc718a76
Corniel Nobel <mail@corniel.nl>
2020-12-05 07:30:49 +0100

    2020-05

---
+++ src/AdventOfCode/2020/Day05.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2020
+{
+    public class Day05
+    {
+        [Puzzle(2020, 05, Part.one)]
+        public static int One(string input)
+            => input.Lines().Select(Seat.Parse).Max(seat => seat.Id);
+
+        [Puzzle(2020, 05, Part.two)]
+        public static int Two(string input)
+        {
+            var seats = input.Lines().Select(Seat.Parse).OrderBy(s => s.Id).ToArray();
+
+            var previous = seats[0];
+
+            foreach (var seat in seats.Skip(1))
+            {
+                if (seat.Id - 1 != previous.Id)
+                {
+                    return seat.Id - 1;
+                }
+                previous = seat;
+            }
+
+            throw new NoAnswer();
+        }
+
+        public readonly struct Seat
+        {
+            public Seat(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+
+            public int Row { get; }
+            public int Col { get; }
+            public int Id => Row * 8 + Col;
+
+            public override string ToString() => $"Row:{Row}, Col: {Col}, ID: {Id}";
+
+            public static Seat Parse(string str)
+            {
+                var row = 0;
+                var col = 0;
+                int r = 64;
+                int c = 4;
+
+                foreach (var ch in str.Take(7))
+                {
+                    if (ch == 'B')
+                    {
+                        row += r;
+                    }
+                    r /= 2;
+                }
+                foreach (var ch in str.Skip(7).Take(3))
+                {
+                    if (ch == 'R')
+                    {
+                        col += c;
+                    }
+                    c /= 2;
+                }
+
+                return new Seat(row, col);
+            }
+
+        }
+
+    }
+}
---
+++ test/AdventOfCode.Tests/2020/day05.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode._2020;
+using AdventOfCode.Tests;
+using NUnit.Framework;
+
+namespace Advent_of_Code_2020
+{
+    public class day05
+    {
+        [TestCase("BFFFBBFRRR", 070, 7, 567)]
+        [TestCase("FFFBBBFRRR", 014, 7, 119)]
+        [TestCase("BBFFBBFRLL", 102, 4, 820)]
+        public void Seats_are_parsed(string str, int row, int col, int id)
+        {
+            var seat = Day05.Seat.Parse(str);
+            Assert.AreEqual(row, seat.Row);
+            Assert.AreEqual(col, seat.Col);
+            Assert.AreEqual(id, seat.Id);
+        }
+
+        [Test]
+        public void part_one()
+        {
+            var input = Input.For(2020, 05);
+            Puzzle.HasAnswer(998, Day05.One, with: input);
+        }
+
+        [Test]
+        public void part_two()
+        {
+            var input = Input.For(2020, 05);
+            Puzzle.HasAnswer(676, Day05.Two, with: input);
+        }
+    }
+}

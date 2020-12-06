3518e30f8e0df610f0a42e462b9f098d06ceebf0
Corniel Nobel <mail@corniel.nl>
2020-12-06 07:24:17 +0100

    2020-06

---
+++ test/AdventOfCode.Tests/2020/day06.cs
@@ -0,0 +1,67 @@
+﻿using AdventOfCode._2020;
+using AdventOfCode.Tests;
+using NUnit.Framework;
+
+namespace Advent_of_Code_2020
+{
+    public class day06
+    {
+        [Test]
+        public void part_one_example()
+        {
+            var input = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+";
+            Puzzle.HasAnswer(11, Day06.One, with: input);
+        }
+
+        [Test]
+        public void part_one()
+        {
+            var input = Input.For(2020, 06);
+            Puzzle.HasAnswer(7110, Day06.One, with: input);
+        }
+  
+        [Test]
+        public void part_two_example()
+        {
+            var input = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+";
+            Puzzle.HasAnswer(6, Day06.Two, with: input);
+        }
+
+        [Test]
+        public void part_two()
+        {
+            var input = Input.For(2020, 06);
+            Puzzle.HasAnswer(3628, Day06.Two, with: input);
+        }
+    }
+}

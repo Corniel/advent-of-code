a9ed11e25dc2ac48a649acc101f6e711b687ef84
Corniel Nobel <mail@corniel.nl>
2020-12-07 10:03:50 +0100

    Introduction of data structure of a tree with leaves with repeating children.

---
+++ src/AdventOfCode/SmartAss/Trees/Leaves.cs
@@ -0,0 +1,39 @@
+﻿using SmartAss.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SmartAss.Trees
+{
+    [DebuggerTypeProxy(typeof(CollectionDebugView))]
+    [DebuggerDisplay("{DebuggerDisplay}")]
+    public class Leaves<TLeaf, TLabel> : IEnumerable<RepeatingLeaf<TLeaf, TLabel>>
+        where TLeaf : Leaf<TLeaf, TLabel>
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly Dictionary<TLabel, RepeatingLeaf<TLeaf, TLabel>> leaves = new();
+
+        public void Add(TLeaf leaf, int repeats)
+        {
+            if (leaves.ContainsKey(leaf.Label))
+            {
+                leaves[leaf.Label] += repeats;
+            }
+            else
+            {
+                leaves[leaf.Label] = new RepeatingLeaf<TLeaf, TLabel>(leaf, repeats);
+            }
+        }
+
+        public void Add(TLeaf leaf) => Add(leaf, 1);
+
+        public IEnumerator<RepeatingLeaf<TLeaf, TLabel>> GetEnumerator()
+            => leaves.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string DebuggerDisplay => $"Count: {leaves.Sum(l => l.Value.Repeats)}, Unique: {leaves.Count}";
+    }
+}
---
+++ src/AdventOfCode/SmartAss/Trees/RepeatingLeaf.cs
@@ -0,0 +1,22 @@
+﻿namespace SmartAss.Trees
+{
+    public readonly struct RepeatingLeaf<TLeaf, TLabel> where TLeaf : Leaf<TLeaf, TLabel>
+    {
+        public RepeatingLeaf(TLeaf leaf, int repeats = 1)
+        {
+            Leaf = leaf;
+            Repeats = repeats;
+        }
+        public TLeaf Leaf { get; }
+
+        public int Repeats { get; }
+
+        public RepeatingLeaf<TLeaf, TLabel> Add(int repeats)
+            => new(Leaf, Repeats + repeats);
+
+        public override string ToString() => $"{Repeats}: {Leaf}";
+
+        public static RepeatingLeaf<TLeaf, TLabel> operator +(RepeatingLeaf<TLeaf, TLabel> leaf, int repeats)
+            => leaf.Add(repeats);
+    }
+}

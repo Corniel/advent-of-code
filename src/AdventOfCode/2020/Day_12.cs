1c0fd6461c052e9cb6ba1e123ae3d53d59a9a57b
Corniel Nobel <mail@corniel.nl>
2020-12-12 19:35:12 +0100

    It's a ferry. :) (And orientation was misspelled).

--- src/AdventOfCode/2020/Day_12.cs
+++ src/AdventOfCode/2020/Day_12.cs
@@ -11,32 +11,32 @@ namespace Advent_of_Code_2020
         [Puzzle(answer: 1631, year: 2020, day: 12)]
         public int part_one(string input)
         {
-            var ship = Point.O;
-            var orrientation = Vector.E;
+            var ferry = Point.O;
+            var orientation = Vector.E;
 
             foreach (var i in input.Lines().Select(Instruction.Parse))
             {
-                if (i.Action == Action.F) { ship += orrientation * i.Distance; }
-                else if (i.Rotation != 0) { orrientation = orrientation.Rotate(i.Rotation); }
-                else { ship += i.Direction * i.Distance; }
+                if (i.Action == Action.F) { ferry += orientation * i.Distance; }
+                else if (i.Rotation != 0) { orientation = orientation.Rotate(i.Rotation); }
+                else { ferry += i.Direction * i.Distance; }
             }
-            return ship.ManhattanDistance(Point.O);
+            return ferry.ManhattanDistance(Point.O);
         }
 
         [Example(answer: 286, "F10;N3;F7;R90;F11")]
         [Puzzle(answer: 58606, year: 2020, day: 12)]
         public int part_two(string input)
         {
-            var ship = Point.O;
+            var ferry = Point.O;
             var waypoint = new Point(+10, -1);
 
             foreach (var i in input.Lines().Select(Instruction.Parse))
             {
-                if (i.Action == Action.F) { ship += (waypoint - Point.O) * i.Distance; }
+                if (i.Action == Action.F) { ferry += (waypoint - Point.O) * i.Distance; }
                 else if (i.Rotation != 0) { waypoint = waypoint.Rotate(Point.O, i.Rotation); }
                 else { waypoint += i.Direction * i.Distance; }
             }
-            return ship.ManhattanDistance(Point.O);
+            return ferry.ManhattanDistance(Point.O);
         }
 
         private readonly struct Instruction
@@ -47,33 +47,25 @@ namespace Advent_of_Code_2020
                 Rotation = action == Action.L || action == Action.R ? value / (int)action : 0;
                 Distance = Rotation == 0 ? value : 0;
             }
-
             public Action Action { get; }
             public int Distance { get; }
             public int Rotation { get; }
-            public Vector Direction
-                => Action switch
-                {
-                    Action.E => Vector.E,
-                    Action.S => Vector.S,
-                    Action.W => Vector.W,
-                    Action.N => Vector.N,
-                    _ => Vector.O,
-                };
-
-            public static Instruction Parse(string str)
-                => new Instruction(Enum.Parse<Action>(str.Substring(0, 1)), str.Substring(1).Int32());
+            public Vector Direction => Action switch
+            {
+                Action.E => Vector.E,
+                Action.S => Vector.S,
+                Action.W => Vector.W,
+                Action.N => Vector.N,
+                _ => Vector.O,
+            };
+            public static Instruction Parse(string str) => new Instruction(Enum.Parse<Action>(str[0..1]), str[1..].Int32());
         }
-
         private enum Action
         {
-            N = 0,
-            E = 1,
-            S = 2,
-            W = 3,
+            N, E, S, W, 
+            F,
             L = +90,
             R = -90,
-            F = int.MaxValue,
         }
     }
 }

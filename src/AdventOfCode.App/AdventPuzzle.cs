ff6af5fb3a26c372fefcbe5b380eac2a0c26119e
Corniel Nobel <mail@corniel.nl>
2020-12-14 18:10:21 +0100

    Extended functionality for aoc console app.

---
+++ src/AdventOfCode.App/AdventPuzzle.cs
@@ -0,0 +1,71 @@
+﻿using SmartAss;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Linq;
+
+namespace Advent_of_Code
+{
+    public class AdventPuzzle
+    {
+        public AdventPuzzle(AdventDate date, MethodInfo method)
+        {
+            Date = date;
+            Method = method;
+            Input = Method.GetCustomAttributes<PuzzleAttribute>().FirstOrDefault(a => a.GetType() == typeof(PuzzleAttribute)).Input;
+        }
+
+        public bool Matches(AdventDate date) => Date.Matches(date);
+
+        public AdventDate Date { get; }
+        private MethodInfo Method { get; }
+        private string Input { get; }
+
+        public void Run()
+        {
+            var test = Activator.CreateInstance(Method.DeclaringType);
+
+            object answer = null;
+            var sw = new Stopwatch();
+            try
+            {
+                sw.Start();
+                answer = Method.Invoke(test, new object[] { Input });
+                sw.Stop();
+            }
+            catch (TargetInvocationException x)
+            {
+                if (x.InnerException is NoAnswer) Do.Nothing();
+                else throw x.InnerException;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(Date);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(": ");
+            Console.ForegroundColor = ConsoleColor.White;
+            if (answer is null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("no answer");
+            }
+            else if (answer is string str)
+            {
+                Console.WriteLine();
+                Console.WriteLine(str);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(answer);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(", ");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            if (answer != null)
+            {
+                Console.WriteLine($"duration: {sw.Elapsed.TotalMilliseconds:0.000} ms ({sw.ElapsedTicks:#,##0} ticks)");
+            }
+        }
+    }
+}
--- src/AdventOfCode.App/Program.cs
+++ src/AdventOfCode.App/Program.cs
@@ -1,8 +1,5 @@
-﻿using SmartAss.Parsing;
-using System;
-using System.Diagnostics;
+﻿using System;
 using System.Linq;
-using System.Reflection;
 
 namespace Advent_of_Code
 {
@@ -14,85 +11,35 @@ namespace Advent_of_Code
         public static int Main(string[] args)
         {
             if (args?.Length < 1) { return Usage(); }
-            if (!AdventDay(args[0], out var year, out var day, out var part)) { return InvalidDay(args[0]); }
-            if (!TestClass(year, day, out var testClass)) { return Generate(year, day); }
-            if (!TestMethod(testClass, part, out var methodInfo)) { return NoMethod(year, day, part); }
 
-            try
+            var puzzles = AdventPuzzles.Load();
+
+            if (!AdventDate.TryParse(args[0], out var date)) { return InvalidDay(args[0]); }
+
+            if (date.SpecifiesYearDay() && !puzzles.Contains(date))
             {
-                var input = Puzzle.Input(year, day);
-                var test = Activator.CreateInstance(testClass);
-                var stopWatch = Stopwatch.StartNew();
-                var answer = methodInfo.Invoke(test, new object[] { input });
-                stopWatch.Stop();
-                Console.WriteLine($"answer: {answer}");
-                Console.WriteLine($"duration: {stopWatch.Elapsed.TotalMilliseconds:0.000} ms ({stopWatch.ElapsedTicks:#,##0} ticks)");
+                return Generate(date);
             }
-            catch(TargetInvocationException x)
+            else
             {
-                if (x.InnerException is NoAnswer noAnswer)
+                var matching = puzzles.Matching(date);
+                if (matching.Any())
                 {
-                    Console.WriteLine(noAnswer.Message);
-                    return Failure;
+
+                    foreach (var puzzle in puzzles.Matching(date))
+                    {
+                        puzzle.Run();
+                    }
+                    return Success;
                 }
-                else throw x.InnerException;
+                else return NoMethod(date);
             }
-            return Success;
         }
 
-        private static int NoMethod(int year, int day, int part)
-        {
-            Console.WriteLine($"No test method found for {year}-{day:00} part {part}");
-            return Failure;
-        }
-
-        private static bool AdventDay(string arg, out int year, out int day, out int part)
-        {
-            day = default;
-            part = default;
-
-            var args = arg.Seperate('-').ToArray();
-            if (!int.TryParse(args[0], out year) ||
-               !int.TryParse(args[1], out day) ||
-               year < 2015 ||
-               day < 0 || day > 25)
-            {
-                return false;
-            }
-            else if (args.Length == 2) { return true; }
-            else if (args.Length == 3)
-            {
-                switch (args[2].ToUpperInvariant())
-                {
-                    case "1":
-                    case "ONE": part = 1; return true;
-                    case "2":
-                    case "TWO": part = 2; return true;
-                    default: return false;
-                }
-            }
-            else { return false; }
-        }
-
-        private static bool TestClass(int year, int day, out Type testClass)
-        {
-            var fullName = $"Advent_of_Code_{year}.Day_{day:00}";
-            testClass = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(a => a.GetExportedTypes())
-                .FirstOrDefault(tp => tp.FullName == fullName);
-            return !(testClass is null);
-        }
-
-        private static bool TestMethod(Type testClass, int part, out MethodInfo tesMethod)
-        {
-            tesMethod = testClass.GetMethod(part == 1 ? "part_one" : "part_two");
-            return !(tesMethod is null);
-        }
-
-        private static int Generate(int year, int day)
+        private static int Generate(AdventDate date)
         {
             var templating = new Templating();
-            var location = templating.Generate(year, day);
+            var location = templating.Generate(date.Year.Value, date.Day.Value);
             Console.WriteLine($"Template code generated at {location.FullName}");
             return Success;
         }
@@ -106,6 +53,12 @@ namespace Advent_of_Code
         private static int InvalidDay(string arg)
         {
             Console.WriteLine($"'{arg}' is not a valid advent day");
+            return Failure;
+        }
+
+        private static int NoMethod(AdventDate date)
+        {
+            Console.WriteLine($"No test method found for {date}");
             return Failure;
         }
     }

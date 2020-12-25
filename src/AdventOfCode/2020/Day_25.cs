ccd98b6f132b3d58176116b741d3991baa8c95df
Corniel Nobel <mail@corniel.nl>
2020-12-25 12:35:44 +0100

    Small simplification.

--- src/AdventOfCode/2020/Day_25.cs
+++ src/AdventOfCode/2020/Day_25.cs
@@ -7,18 +7,18 @@ namespace Advent_of_Code_2020
 {
     public class Day_25
     {
-        [TestCase(08, 7, 05764801)]
-        [TestCase(11, 7, 17807724)]
-        public void Loop_size_is(int loopSize, long subjectNumber, long publicKey)
+        [TestCase(08, 05764801)]
+        [TestCase(11, 17807724)]
+        public void Loop_size_is(int loopSize, long publicKey)
         {
-            Assert.AreEqual(loopSize, LoopSize(subjectNumber, publicKey));
+            Assert.AreEqual(loopSize, LoopSize(publicKey));
         }
 
         [TestCase(14897079, 17807724, 8)]
         [TestCase(14897079, 5764801, 11)]
-        public void Encryption_key_is(long encryptionKey, long publicKey, int loopsize)
+        public void Encryption_key_is(long key, long subject, int loops)
         {
-            Assert.AreEqual(encryptionKey, EncryptionKey(publicKey, loopsize));
+            Assert.AreEqual(key, EncryptionKey(subject, loops));
         }
 
         [Example(answer: 14897079, input: "5764801\r\n17807724")]
@@ -26,34 +26,27 @@ namespace Advent_of_Code_2020
         public long part_one(string input)
         {
             var numbers = input.Int32s().ToArray();
-            var encryptionKey0 = EncryptionKey(numbers[0], LoopSize(7, numbers[1]));
-            var encryptionKey1 = EncryptionKey(numbers[1], LoopSize(7, numbers[0]));
-            Assert.AreEqual(encryptionKey0, encryptionKey1);
-            return encryptionKey1;
-        }
-
-        public static int LoopSize(long subjectNumber, long publicKey)
-        {
-            var loop = 0;
-            long value = 1;
-            while(value != publicKey)
-            {
-                value = (value * subjectNumber) % 20201227;
-                loop++;
-            }
-            return loop;
-        }
-        public static long EncryptionKey(long publicKey, int loops)
-        {
-            long key = 1;
-            foreach (var loop in Enumerable.Range(0, loops))
-            {
-                key = (key * publicKey) % 20201227;
-            }
-            return key;
+            var key0 = EncryptionKey(numbers[0], LoopSize(numbers[1]));
+            var key1 = EncryptionKey(numbers[1], LoopSize(numbers[0]));
+            Assert.AreEqual(key0, key1);
+            return key1;
         }
 
         [Puzzle(answer: "You only have to pay 49 stars", input: "You only have to pay 49 stars")]
         public string part_two(string input) => input;
+
+        private static int LoopSize(long key)
+        {
+            var loop = 0;
+            long val = 1;
+            for (; val != key; loop++) { val = (val * 7) % 20201227; }
+            return loop;
+        }
+        private static long EncryptionKey(long subject, int loops)
+        {
+            long key = 1;
+            for (var i = 0; i < loops; i++) { key = (key * subject) % 20201227; }
+            return key;
+        }
     }
 }

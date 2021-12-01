28b77e97ff9cb374687b0fa3f53ec15334fb05cf
Corniel Nobel <mail@corniel.nl>
2021-12-01 10:06:06 +0100

    2021-01

--- src/AdventOfCode/2021/Day_01.cs
+++ src/AdventOfCode/2021/Day_01.cs
@@ -2,17 +2,34 @@ namespace Advent_of_Code_2021;
 
 public class Day_01
 {
-    [Example(answer: 666, @"666")]
-    [Puzzle(answer: 666, year: 2021, day: 01)]
-    public long part_one(string input)
-    {
-        throw new NoAnswer();
-    }
+    [Example(answer: 7, @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263")]
+    [Puzzle(answer: 1292, year: 2021, day: 01)]
+    public int part_one(string input)
+        =>  input.Int32s().SelectWithPrevious().Count(t => t.Previous < t.Current);
 
-    [Example(answer: 666, @"666")]
-    [Puzzle(answer: 666, year: 2021, day: 01)]
-    public long part_two(string input)
-    {
-        throw new NoAnswer();
-    }
+    [Example(answer: 5, @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263")]
+    [Puzzle(answer: 1262, year: 2021, day: 01)]
+    public int part_two(string input)
+        => input.Int32s()
+        .SelectWithPrevious(size: 3)
+        .Select(group => group.Sum())
+        .SelectWithPrevious().Count(t => t.Previous < t.Current);
 }
---
+++ src/AdventOfCode/SmartAss/LoopWithPrevious.cs
@@ -0,0 +1,50 @@
+﻿namespace SmartAss;
+
+public static class LoopWithPrevious
+{
+    public static IEnumerable<CurrentAndPrevious<T>> SelectWithPrevious<T>(this IEnumerable<T> items)
+    {
+        var iterator = items.GetEnumerator();
+        iterator.MoveNext();
+        var previous = iterator.Current;
+
+        while (iterator.MoveNext())
+        {
+            yield return new(previous, iterator.Current);
+            previous = iterator.Current;
+        }
+    }
+
+    public static IEnumerable<IReadOnlyList<T>> SelectWithPrevious<T>(this IEnumerable<T> items, int size)
+    {
+        var iterator = items.GetEnumerator();
+        var previous = new T[size];
+        var init = 0;
+
+        while (++init < size && iterator.MoveNext())
+        {
+            previous[init] = iterator.Current;
+        }
+
+        while (iterator.MoveNext())
+        {
+            var current = new T[size];
+            Array.Copy(previous, 1, current, 0, size - 1);
+            current[^1] = iterator.Current;
+            yield return current;
+            previous = current;
+        }
+    }
+}
+
+public readonly struct CurrentAndPrevious<T>
+{
+    public CurrentAndPrevious(T prev, T curr)
+    {
+        Previous = prev;
+        Current = curr;
+    }
+
+    public readonly T Previous;
+    public readonly T Current;
+}

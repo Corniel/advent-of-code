3b7cd740d971ca9c315de7b26aaab0f0144b5547
Corniel Nobel <mail@corniel.nl>
2021-12-05 10:46:08 +0100

    2021-05

---
+++ src/AdventOfCode/2021/Day_05.cs
@@ -0,0 +1,59 @@
+namespace Advent_of_Code_2021;
+
+public class Day_05
+{
+    private const string Example = @"0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
+
+    [Test]
+    public void Parse_dx()
+        => Select("9,7 -> 7,7").Should().BeEquivalentTo(new[] { new Point(9, 7), new Point(8, 7), new Point(7, 7) });
+    
+    [Test]
+    public void Parse_dy()
+        => Select("1,1 -> 1,3").Should().BeEquivalentTo(new[] { new Point(1, 1), new Point(1, 2), new Point(1, 3) });
+    
+    [Test]
+    public void Parse_dxy()
+        => Select("9,7 -> 7,9").Should().BeEquivalentTo(new[] { new Point(9, 7), new Point(8, 8), new Point(7, 9) });
+
+    [Example(answer: 5, Example)]
+    [Puzzle(answer: 6666, year: 2021, day: 05)]
+    public int part_one(string input) => Run(input, diagonal: false);
+  
+    [Example(answer: 12, Example)]
+    [Puzzle(answer: 19081, year: 2021, day: 05)]
+    public int part_two(string input) => Run(input, diagonal: true);
+
+    private static int Run(string input, bool diagonal)
+    {
+        var area = new Dictionary<Point, int>();
+        foreach (var point in input.Lines(line => Select(line, diagonal)).SelectMany(p => p))
+        {
+            if (area.ContainsKey(point))
+            { area[point]++; }
+            else { area[point] = 1; }
+        }
+        return area.Count(kvp => kvp.Value >= 2);
+    }
+
+    public static IEnumerable<Point> Select(string line, bool diagonal = true)
+    {
+        var parts = line.Split(" -> ");
+        var start = Point.Parse(parts[0]);
+        var end = Point.Parse(parts[1]);
+        var delta = (end - start).Sign();
+
+        return diagonal || delta.X == 0 || delta.Y == 0
+            ? (start - delta).Repeat(delta).TakeWhile(point => point != end + delta)
+            : Array.Empty<Point>();
+    }
+}
--- src/AdventOfCode/Properties/GlobalUsings.cs
+++ src/AdventOfCode/Properties/GlobalUsings.cs
@@ -20,3 +20,4 @@ global using System.IO;
 global using System.Linq;
 global using System.Text;
 global using System.Text.RegularExpressions;
+global using FluentAssertions;

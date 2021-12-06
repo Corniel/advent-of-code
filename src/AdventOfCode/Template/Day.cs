c18e5ac7b79d8d9d7d35ffb01280f41d2ce00f14
Corniel Nobel <mail@corniel.nl>
2021-12-06 21:13:43 +0100

    2021-06
    Typoe

--- src/AdventOfCode/Template/Day.cs
+++ src/AdventOfCode/Template/Day.cs
@@ -2,14 +2,16 @@
 
 public class Day_@Day
 {
-    [Example(answer: 666, @"666")]
+    private const string Example = @"";
+
+    [Example(answer: 666, Example)]
     [Puzzle(answer: 666, year: @Year, day: @Day)]
     public long part_one(string input)
     {
         throw new NoAnswer();
     }
 
-    [Example(answer: 666, @"666")]
+    [Example(answer: 666, Example)]
     [Puzzle(answer: 666, year: @Year, day: @Day)]
     public long part_two(string input)
     {

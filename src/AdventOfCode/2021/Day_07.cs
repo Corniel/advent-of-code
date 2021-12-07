d7c0dfe63701bd58655b3b2a343658dc00da2923
Corniel Nobel <mail@corniel.nl>
2021-12-07 08:58:57 +0100

    2021-07

---
+++ src/AdventOfCode/2021/Day_07.cs
@@ -0,0 +1,25 @@
+namespace Advent_of_Code_2021;
+
+public class Day_07
+{
+    [Example(answer: 37, "16,1,2,0,4,2,7,1,2,14")]
+    [Puzzle(answer: 336701, year: 2021, day: 07)]
+    public int part_one(string input) => MinimumCosts(input.Int32s().ToArray(), n => n);
+
+    [Example(answer: 168, "16,1,2,0,4,2,7,1,2,14")]
+    [Puzzle(answer: 95167302, year: 2021, day: 07)]
+    public int part_two(string input) => MinimumCosts(input.Int32s().ToArray(), n => n * (n + 1) / 2);
+
+    private static int MinimumCosts(int[] crabs, Func<int, int> costs)
+    {
+        var optimum = int.MaxValue;
+        var height = 0;
+        while (height++ < int.MaxValue)
+        {
+            var fuel = crabs.Sum(crab => costs(Math.Abs(crab - height)));
+            if (fuel > optimum) return optimum;
+            else { optimum = fuel; }
+        }
+        throw new InfiniteLoop();
+    }
+}

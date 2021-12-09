534fa101bea3fbfa8c9b9ff6f5d84188f9ce8b08
Corniel Nobel <mail@corniel.nl>
2021-12-09 11:14:35 +0100

    2018-02

---
+++ src/AdventOfCode/2018/Day_02.cs
@@ -0,0 +1,43 @@
+namespace Advent_of_Code_2018;
+
+public class Day_02
+{
+    [Puzzle(answer: 5928, year: 2018, day: 02)]
+    public int part_one(string input)
+        => input.Lines().Count(line => Repeat(line, 2))
+        * input.Lines().Count(line => Repeat(line, 3));
+
+
+    static bool Repeat(string line, int n) => line.Any(ch => line.Count(c => c == ch) == n);
+
+    [Example(answer: "fgij", "abcde;fghij;klmno;pqrst;fguij;axcye;wvxyz")]
+    [Puzzle(answer: "bqlporuexkwzyabnmgjqctvfs", year: 2018, day: 02)]
+    public string part_two(string input)
+    {
+        var lines = input.Lines();
+        
+        for(var f = 0; f < lines.Count; f++)
+        {
+            for(var s = f+1; s < lines.Count; s++)
+            {
+                if (Matching(lines[f], lines[s]) is { } match) return match;
+            }
+        }
+        throw new NoAnswer();
+    }
+
+    static string Matching(string f, string s)
+    {
+        var matching = new char[f.Length -1];
+        var missing = 0;
+        for(var i = 0; i < f.Length; i++)
+        {
+            if (f[i] == s[i])
+            {
+                matching[i - missing] = f[i];
+            }
+            else if (missing++ > 0) return null;
+        }
+        return new(matching);
+    }
+}

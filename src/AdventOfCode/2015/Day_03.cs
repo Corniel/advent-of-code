5fce1f99a153a2a3736b682bb507124936312c4d
Corniel Nobel <mail@corniel.nl>
2021-12-09 14:51:30 +0100

    2015-03

---
+++ src/AdventOfCode/2015/Day_03.cs
@@ -0,0 +1,54 @@
+namespace Advent_of_Code_2015;
+
+public class Day_03
+{
+    private const string Example = @"";
+
+    [Example(answer: 4, "^>v<")]
+    [Puzzle(answer: 2592, year: 2015, day: 03)]
+    public int part_one(string input)
+    {
+        var done = new HashSet<Point> { Point.O };
+        var santa = Point.O;
+        foreach(var dir in input.Select(Parse))
+        {
+            santa += dir;
+            done.Add(santa);
+        }
+        return done.Count;
+    }
+
+    [Example(answer: 11, "^v^v^v^v^v")]
+    [Puzzle(answer: 2360, year: 2015, day: 03)]
+    public int part_two(string input)
+    {
+        var done = new HashSet<Point> { Point.O };
+        var santa = Point.O;
+        var robo = Point.O;
+        var first = true;
+        foreach (var dir in input.Select(Parse))
+        {
+            if (first)
+            {
+                santa += dir;
+                done.Add(santa);
+            }
+            else
+            {
+                robo += dir;
+                done.Add(robo);
+            }
+            first = !first;
+        }
+        return done.Count;
+    }
+
+    private static Vector Parse(char ch)
+    => ch switch 
+    {
+        '^' => Vector.N,
+        '>' => Vector.E,
+        '<' => Vector.W,
+        _ => Vector.S,
+    };
+}

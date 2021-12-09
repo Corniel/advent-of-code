5361c81e9013d164193f39d77e35e5964e1b0d8f
Corniel Nobel <mail@corniel.nl>
2021-12-09 11:01:35 +0100

    2018-01

---
+++ src/AdventOfCode/2018/Day_01.cs
@@ -0,0 +1,28 @@
+namespace Advent_of_Code_2018;
+
+public class Day_01
+{
+    [Example(answer: 3, "+1, +1, +1")]
+    [Example(answer: 0, "+1, +1, -2")]
+    [Puzzle(answer: 538, year: 2018, day: 01)]
+    public int part_one(string input) => input.Int32s().Sum();
+
+    [Example(answer: 0, "+1, -1")]
+    [Example(answer: 10, "+3, +3, +4, -2, -4")]
+    [Example(answer: 14, "+7, +7, -2, -7, -4")]
+    [Puzzle(answer: 77271, year: 2018, day: 01)]
+    public int part_two(string input)
+    {
+        var frequencies = new HashSet<int>{ 0 };
+        var current = 0;
+        while (true)
+        {
+            foreach (var f in input.Int32s())
+            {
+                current += f;
+                if (!frequencies.Add(current)) return current;
+            }
+        }
+        throw new InfiniteLoop();
+    }
+}

d440c6319ffca7be0b583c3acc523e7b38f7cd5b
Corniel Nobel <mail@corniel.nl>
2021-12-09 15:16:58 +0100

    2015-04

---
+++ src/AdventOfCode/2015/Day_04.cs
@@ -0,0 +1,40 @@
+using System.Security.Cryptography;
+
+namespace Advent_of_Code_2015;
+
+public class Day_04
+{
+    
+    [Example(answer: 609043, "abcdef")]
+    [Puzzle(answer: 346386, "iwrupvqb")]
+    public int part_one(string input) => Run(input, FiveZeros);
+
+    [Puzzle(answer: 9958218, "iwrupvqb")]
+    public int part_two(string input) => Run(input, SixZeros);
+
+    private static readonly HashAlgorithm Hash = MD5.Create();
+
+    private static int Run(string input, Func<byte[], bool> condition)
+    {
+        for (var n = 0; n < int.MaxValue; n++)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input + n.ToString());
+            var hashed = Hash.ComputeHash(bytes);
+            if (condition(hashed))
+            {
+                return n;
+            }
+        }
+        throw new InfiniteLoop();
+    }
+
+    private static bool FiveZeros(byte[] hashed)
+        => hashed[0] == 0 
+        && hashed[1] == 0 
+        && hashed[2] != 0 && hashed[2] <= 15;
+
+    private static bool SixZeros(byte[] hashed)
+        => hashed[0] == 0
+        && hashed[1] == 0
+        && hashed[2] == 0;
+}

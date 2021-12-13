0646a0afbbdb6d313cd11d4efecea006d7582d1f
Corniel Nobel <mail@corniel.nl>
2021-12-13 21:16:03 +0100

    Introduction of ASCII Text

---
+++ src/AdventOfCode/Extensions/SmartAss.Grid.cs
@@ -0,0 +1,6 @@
+﻿namespace SmartAss;
+
+public static class AoCGridExtensions
+{
+    public static string AsciiText(this Grid<bool> grid, bool trim = false) => SmartAss.AsciiText.Parse(grid, trim);
+}
---
+++ src/AdventOfCode/SmartAss/AsciiText.cs
@@ -0,0 +1,98 @@
+﻿namespace SmartAss;
+
+public static class AsciiText
+{
+    public static string Parse(Grid<bool> grid, bool trim = false)
+    {
+        if (grid.Rows != 6) throw new ArgumentException("Should contain 6 rows", nameof(grid));
+        try
+        {
+            var chs = new TextIterator(grid, trim).Select(bits => Lookup[bits]).ToArray();
+            return new string(chs, 0, chs.Length).Trim();
+        }
+        catch (KeyNotFoundException)
+        {
+            throw new FormatException($"Not a valid ASCII text:\r\n{grid.ToString(b => b ? '█' : '░')}");
+        }
+    }
+
+    private static readonly Dictionary<ulong, char> Lookup = Init();
+
+    private static Dictionary<ulong, char> Init()
+    {
+        var grid = @"
+░██░░███░░░██░░███░░████░████░░██░░█░░█░░███░░░██░█░░█░█░░░░█░░░██░░█░░██░░███░░░██░░███░░░███░░███░█░░█░█░░░██░░░██░░░██░░░█████░
+█░░█░█░░█░█░░█░█░░█░█░░░░█░░░░█░░█░█░░█░░░█░░░░░█░█░█░░█░░░░██░████░█░█░░█░█░░█░█░░█░█░░█░█░░░░░░█░░█░░█░█░░░██░░░█░█░█░█░░░█░░░█░
+█░░█░███░░█░░░░█░░█░███░░███░░█░░░░████░░░█░░░░░█░██░░░█░░░░██░████░█░█░░█░█░░█░█░░█░█░░█░█░░░░░░█░░█░░█░░█░█░█░░░█░░█░░░█░█░░░█░░
+████░█░░█░█░░░░█░░█░█░░░░█░░░░█░██░█░░█░░░█░░░░░█░█░█░░█░░░░█░█░██░██░█░░█░███░░█░░█░███░░░██░░░░█░░█░░█░░█░█░█░█░█░░█░░░░█░░░█░░░
+█░░█░█░░█░█░░█░█░░█░█░░░░█░░░░█░░█░█░░█░░░█░░█░░█░█░█░░█░░░░█░█░██░██░█░░█░█░░░░█░█░░█░█░░░░░█░░░█░░█░░█░░░█░░█░█░█░█░█░░░█░░█░░░░
+█░░█░███░░░██░░███░░████░█░░░░░███░█░░█░░███░░██░░█░░█░████░█░░░██░░█░░██░░█░░░░░█░█░█░░█░███░░░░█░░░██░░░░█░░░█░█░█░░░█░░█░░████░".CharPixels().Grid(ch => ch == '█');
+
+        var lookup = new Dictionary<ulong, char>();
+        var ch = 'A';
+        foreach(var bits in new TextIterator(grid, false))
+        {
+            lookup[bits] = ch++;
+        }
+        lookup[0] = ' ';
+        return lookup;
+    }
+
+    private struct TextIterator : Iterator<ulong>
+    {
+        private readonly Grid<bool> Grid;
+        private bool Trim;
+        private int Col;
+
+        public TextIterator(Grid<bool> grid, bool trim)
+        {
+            Grid = grid;
+            Col = 0;
+            Current = 0;
+            Trim = trim;
+        }
+
+        public ulong Current { get; private set; }
+
+        public bool MoveNext()
+        {
+            TrimEmptyColumns();
+
+            if (Grid.Cols - Col >= 4)
+            {
+                Current = 0;
+                for (var row = 0; row < 6; row++)
+                {
+                    for (var d_c = 0; d_c < 5; d_c++)
+                    {
+                        Current <<= 1;
+                        var col = Col + d_c;
+                        if (col != Grid.Cols) { Current |= Grid[col, row] ? 1UL : 0UL; }
+                    }
+                }
+                Col += 5;
+                return true;
+            }
+            return false;
+        }
+
+        private void TrimEmptyColumns()
+        {
+            while (Trim)
+            {
+                for (var row = 0; row < 6; row++)
+                {
+                    if (Grid[Col, row])
+                    {
+                        Trim = false;
+                        return;
+                    }
+                }
+                Col++;
+            }
+        }
+
+        public void Dispose() => Do.Nothing();
+        public void Reset() => throw new NotSupportedException();
+    }
+}

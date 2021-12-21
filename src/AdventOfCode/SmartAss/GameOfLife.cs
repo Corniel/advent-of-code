36607bb002866bc74af38a4a7d778ef129bde0d6
Corniel Nobel <mail@corniel.nl>
2021-12-21 20:45:44 +0100

    GameOfLife with more options

--- src/AdventOfCode/SmartAss/GameOfLife.cs
+++ src/AdventOfCode/SmartAss/GameOfLife.cs
@@ -4,16 +4,22 @@
 public abstract class GameOfLife<TCell> : HashSet<TCell>
 {
     /// <summary>Returns true when a living cell should die.</summary>
-    public abstract bool Dies(int living);
+    protected virtual bool Dies(TCell cell) => Dies(LivingNeighbors(cell));
+
+    /// <summary>Returns true when a living cell should die.</summary>
+    protected virtual bool Dies(int living) => living < 2 || living > 3;
 
     /// <summary>Returns true when a non-living cell should come into existence.</summary>
-    public abstract bool IntoExistence(int living);
+    protected virtual bool IntoExistence(TCell cell) => IntoExistence(LivingNeighbors(cell));
+    
+    /// <summary>Returns true when a non-living cell should come into existence.</summary>
+    protected virtual bool IntoExistence(int living) => living == 3;
 
     /// <summary>Gets the neighbors of a cell.</summary>
     public abstract IEnumerable<TCell> Neighbors(TCell cell);
 
     /// <summary>Simulates multiple generations.</summary>
-    public void Generations(int generations)
+    public virtual void Generations(int generations)
     {
         foreach (var _ in Enumerable.Range(0, generations))
         {
@@ -29,8 +35,8 @@ public abstract class GameOfLife<TCell> : HashSet<TCell>
         intoExistance.Clear();
 
         candidates.AddRange(this.SelectMany(alive => Neighbors(alive)));
-        toDie.AddRange(this.Where(cell => Dies(LivingNeighbors(cell))));
-        intoExistance.AddRange(candidates.Where(cell => IntoExistence(LivingNeighbors(cell))));
+        toDie.AddRange(this.Where(Dies));
+        intoExistance.AddRange(candidates.Where(IntoExistence));
 
         foreach (var dead in toDie) { Remove(dead); }
         foreach (var alive in intoExistance) { Add(alive); }

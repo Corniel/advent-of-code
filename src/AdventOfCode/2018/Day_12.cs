86bb506a1b22add619e47385628e30ebc06730da
Corniel Nobel <mail@corniel.nl>
2022-11-16 21:12:57 +0100

    2018-12

---
+++ src/AdventOfCode/2018/Day_12.cs
@@ -0,0 +1,57 @@
+using SmartAss.Numerics;
+
+namespace Advent_of_Code_2018;
+
+public class Day_12
+{
+    [Example(answer: 325, year: 2018, day: 12, example: 1)]
+    [Puzzle(answer: 3605, year: 2018, day: 12)]
+    public long part_one(string input)
+    {
+        var game = new GameOfLife(input);
+        game.Generations(20);
+        return game.Sum();
+    }
+
+    [Puzzle(answer: 4050000000798, year: 2018, day: 12)]
+    public long part_two(string input)
+    {
+        var game = new GameOfLife(input);
+        game.Generations(1000);
+        return game.Sum() + (50_000_000_000 - 1000) * game.Count;
+    }
+
+    class GameOfLife : GameOfLife<long>
+    {
+        public GameOfLife(string input)
+        {
+            foreach (var mask in input.Lines().Skip(1).Select(line => Bits.UInt32.Parse(line, "#", ".")))
+            {
+                nexts[mask >> 1] = (mask & 1) == 1;
+            }
+            var index = 0;
+            foreach (var ch in input.Lines().First().Where(ch => "#.".Contains(ch)))
+            {
+                if (ch == '#') Add(index);
+                index++;
+            }
+        }
+
+        private readonly bool[] nexts = new bool[0b10_00_00];
+
+        protected override bool Dies(long cell) => !IntoExistence(cell);
+
+        protected override bool IntoExistence(long cell)
+        {
+            var pattern = 0;
+            pattern |= Contains(cell + 2) ? 01 : 0;
+            pattern |= Contains(cell + 1) ? 02 : 0;
+            pattern |= Contains(cell + 0) ? 04 : 0;
+            pattern |= Contains(cell - 1) ? 08 : 0;
+            pattern |= Contains(cell - 2) ? 16 : 0;
+            return nexts[pattern];
+        }
+
+        public override IEnumerable<long> Neighbors(long cell) => Enumerable.Range((int)cell - 2, 5).Where(c => c != cell).Select(c => (long)c);
+    }
+}

b5148f039877bac301ef3eaf49745a7df4f21962
Corniel Nobel <mail@corniel.nl>
2022-11-21 13:33:20 +0100

    Adjustments to new JSON structure (Id has become a number).

--- src/AdventOfCode.Utils/Rankings/Json/Member.cs
+++ src/AdventOfCode.Utils/Rankings/Json/Member.cs
@@ -5,8 +5,7 @@ namespace Advent_of_Code.Rankings.Json;
 public class Member
 {
     [JsonPropertyName("id")]
-    public string id { get; set; }
-    public long Id => id.Int64();
+    public long Id { get; set; }
     [JsonPropertyName("name")]
     public string Name { get; set; }
     [JsonPropertyName("global_score")]

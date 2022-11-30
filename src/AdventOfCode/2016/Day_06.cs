3974cffbc3392c131fcc79c83538c099a80a9809
Corniel Nobel <mail@corniel.nl>
2022-11-30 12:46:58 +0100

    2016-06

---
+++ src/AdventOfCode/2016/Day_06.cs
@@ -0,0 +1,18 @@
+namespace Advent_of_Code_2016;
+
+[Category(Category.Cryptography)]
+public class Day_06
+{
+    [Example(answer: "easter", "eedadn;drvtee;eandsr;raavrd;atevrs;tsrnev;sdttsa;rasrtv;nssdts;ntnada;svetve;tesnvt;vntsnd;vrdear;dvrsen;enarar")]
+    [Puzzle(answer: "mshjnduc")]
+    public string part_one(string input) => Decrypt(input.Lines().ToArray(), Max);
+
+    [Puzzle(answer: "apfeeebz")]
+    public string part_two(string input) => Decrypt(input.Lines().ToArray(), Min);
+
+    private static string Decrypt(string[] lines, Func<IEnumerable<char>, char> selector) 
+        => new(Enumerable.Range(0, lines[0].Length).Select(i => selector(lines.Select(line => line[i]))).ToArray());
+
+    static char Max(IEnumerable<char> chars) => new ItemCounter<char> { chars }.Max().Item;
+    static char Min(IEnumerable<char> chars) => new ItemCounter<char> { chars }.Min().Item;
+}

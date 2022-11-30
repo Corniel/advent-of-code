fb424d41e70c2675823d5c050d887a1676f2b5d5
Corniel Nobel <mail@corniel.nl>
2022-11-30 17:52:49 +0100

    2016-07

---
+++ src/AdventOfCode/2016/Day_07.cs
@@ -0,0 +1,50 @@
+namespace Advent_of_Code_2016;
+
+[Category(Category.Cryptography)]
+public class Day_07
+{
+    [Example(answer: 0, "aaaa[qwer]tyui")]
+    [Example(answer: 0, "abcd[bddb]xyyx")]
+    [Example(answer: 1, "abba[mnop]qrst")]
+    [Example(answer: 1, "ioxxoj[asdfgh]zxcvbn")]
+    [Puzzle(answer: 105)]
+    public int part_one(string input) => input.Lines().Count(SupportsTls);
+
+    [Example(answer: 0, "xyx[xyx]xyx")]
+    [Example(answer: 1, "aba[bab]xyz")]
+    [Example(answer: 1, "zazbz[bzb]cdb")]
+    [Puzzle(answer: 258)]
+    public int part_two(string input) => input.Lines().Count(SupportsSsl);
+
+    static bool SupportsTls(string line)
+    {
+        var support = false;
+        var odd = true;
+        foreach (var block in line.Seperate('[', ']'))
+        {
+            if (odd) support |= ABBA(block);
+            else if (ABBA(block)) return false;
+            odd = !odd;
+        }
+        return support;
+    }
+
+    static bool ABBA(string str) => str.SelectWithPrevious(4).Any(block
+       => block[0] == block[3]
+       && block[1] == block[2]
+       && block[0] != block[1]);
+
+    static bool SupportsSsl(string line)
+    {
+        var blocks = line.Seperate('[', ']');
+        var odds = blocks.WithStep(2).SelectMany(line => line.SelectWithPrevious(3));
+        var evens = blocks.Skip(1).WithStep(2).SelectMany(line => line.SelectWithPrevious(3)).ToArray();
+        return odds.Any(odd => evens.Any(even => ABA_BAB(odd, even)));
+    }
+
+    static bool ABA_BAB(string l, string r)
+        => r[1] == l[0] && r[1] == l[2]
+        && l[1] == r[0] && l[1] == r[2]
+        && l[0] != l[1];
+}
+

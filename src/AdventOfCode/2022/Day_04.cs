dde34f54799c205c812c1e243f3af2acbf97bae9
Corniel Nobel <mail@corniel.nl>
2022-12-04 10:21:50 +0100

    Use Int32Range.

--- src/AdventOfCode/2022/Day_04.cs
+++ src/AdventOfCode/2022/Day_04.cs
@@ -5,20 +5,11 @@ public class Day_04
 {
     [Example(answer: 2, "2-4,6-8;2-3,4-5;5-7,7-9;2-8,3-7;6-6,4-6;2-6,4-8")]
     [Puzzle(answer: 433)]
-    public int part_one(string input) => input.Lines(Pair.Parse).Count(ps => ps[0].F == ps[1].F || ps[0].S >= ps[1].S);
+    public int part_one(string input) => input.Lines(Parse).Count(ps => ps[0].FullyContains(ps[1]) || ps[1].FullyContains(ps[0]));
 
     [Example(answer: 4, "2-4,6-8;2-3,4-5;5-7,7-9;2-8,3-7;6-6,4-6;2-6,4-8")]
     [Puzzle(answer: 852)]
-    public long part_two(string input) => input.Lines(Pair.Parse).Count(ps => ps[0].S >= ps[1].F);
+    public long part_two(string input) => input.Lines(Parse).Count(ps => ps[0].Overlaps(ps[1]));
 
-    record Pair(int F, int S)
-    {
-        public static Pair[] Parse(string line)
-        {
-            var ns = line.Separate(',', '-').Int32s().ToArray();
-            var p1 = new Pair(ns[0], ns[1]);
-            var p2 = new Pair(ns[2], ns[3]);
-            return p1.F <= p2.F ? new[] { p1, p2 } : new[] { p2, p1 };
-        }
-    }
+    static Int32Range[] Parse(string line) => line.CommaSeparated().Select(Int32Range.Parse).ToArray();
 }

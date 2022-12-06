3b0f02a8c9fa60e1f156ad9856a02950eb7ab26e
Corniel Nobel <mail@corniel.nl>
2022-12-06 21:03:29 +0100

    Detail rankings.

---
+++ src/AdventOfCode.Utils/Rankings/DetailRanking.TValue.cs
@@ -0,0 +1,32 @@
+﻿namespace Advent_of_Code.Rankings;
+
+public sealed record DetailRanking<TValue>(Participant Participant, IReadOnlyCollection<Score<TValue>> Scores, int Position = 0) : IFormattable
+    where TValue : struct, IComparable<TValue>, System.Numerics.IAdditionOperators<TValue, TValue, TValue>, IFormattable
+{
+    public TValue Score => Scores.Sum(s => s.Value);
+
+    public override string ToString() => ToString(null, null);
+
+    public string ToString(string format, IFormatProvider formatProvider)
+    {
+        var sb = new StringBuilder();
+        sb.Append($"{Position,3}) ");
+        sb.Append($"{Score,5} ");
+
+        for (var day = 1; day <= 25; day++)
+        {
+            Append(sb, Scores.FirstOrDefault(s => s.Date.Day == day && s.Date.Part == 1));
+            Append(sb, Scores.FirstOrDefault(s => s.Date.Day == day && s.Date.Part == 2));
+        }
+        sb.Append($"  {Participant.ToString(format, formatProvider)}");
+        return sb.ToString();
+
+        static void Append(StringBuilder sb, Score<TValue> score)
+        {
+            if (score is null) sb.Append('.');
+            else if(score.Rank > 9) sb.Append('*');
+            else sb.Append(score.Rank);
+        }
+    }
+}
+

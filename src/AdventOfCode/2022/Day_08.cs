317cebcafc5ee0ddc0ca22cfe7a19d1a90985659
Corniel Nobel <mail@corniel.nl>
2022-12-08 08:10:39 +0100

    2020-08

---
+++ src/AdventOfCode/2022/Day_08.cs
@@ -0,0 +1,39 @@
+﻿namespace Advent_of_Code_2022;
+
+[Category(Category.ms, Category.Grid, Category.VectorAlgebra)]
+public class Day_08
+{
+    [Example(answer: 21, "30373\r\n25512\r\n65332\r\n33549\r\n35390")]
+    [Puzzle(answer: 1711)]
+    public int part_one(string input)
+    {
+        var map = input.CharPixels().Grid();
+        return map.Positions.Count(p => map.OnEdge(p)
+            || IsVisible(p, Vector.N, map)
+            || IsVisible(p, Vector.E, map)
+            || IsVisible(p, Vector.S, map)
+            || IsVisible(p, Vector.W, map));
+    }
+    static bool IsVisible(Point p, Vector dir, Grid<char> map) => p.Repeat(dir).TakeWhile(map.OnGrid).All(o => map[o] < map[p]);
+
+    [Example(answer: 8, "30373\r\n25512\r\n65332\r\n33549\r\n35390")]
+    [Puzzle(answer: 301392)]
+    public long part_two(string input)
+    {
+        var map = input.CharPixels().Grid();
+        return map.Positions.Where(p => !map.OnEdge(p)).Select(p => Scenic(p, map)).Max();
+    }
+
+    static int Scenic(Point p, Grid<char> map) => View(p, Vector.N, map) * View(p, Vector.E, map) * View(p, Vector.S, map) * View(p, Vector.W, map);
+
+    static int View(Point p, Vector dir, Grid<char> map)
+    {
+        var score = 0;
+        foreach (var h in p.Repeat(dir).TakeWhile(map.OnGrid).Select(n => map[n]))
+        {
+            score++;
+            if (h >= map[p]) return score;
+        }
+        return score;
+    }
+}

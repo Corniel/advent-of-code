69f6bd3bf347250acdd773a529ff2517d9a2c848
Corniel Nobel <mail@corniel.nl>
2022-12-09 10:55:19 +0100

    Reduce LoC 2022-09
    shorten

--- src/AdventOfCode/2022/Day_09.cs
+++ src/AdventOfCode/2022/Day_09.cs
@@ -3,12 +3,12 @@
 [Category(Category.ms, Category.Grid, Category.VectorAlgebra)]
 public class Day_09
 {
-    [Example(answer: 13, "R 4\r\nU 4\r\nL 3\r\nD 1\r\nR 4\r\nD 1\r\nL 5\r\nR 2")]
+    [Example(answer: 13, "R 4;U 4;L 3;D 1;R 4;D 1;L 5;R 2")]
     [Puzzle(answer: 6018)]
     public int part_one(string input) => Simulate(input, 2);
 
-    [Example(answer: 1, "R 4\r\nU 4\r\nL 3\r\nD 1\r\nR 4\r\nD 1\r\nL 5\r\nR 2")]
-    [Example(answer: 36, "R 5\r\nU 8\r\nL 8\r\nD 3\r\nR 17\r\nD 10\r\nL 25\r\nU 20")]
+    [Example(answer: 1, "R 4;U 4;L 3;D 1;R 4;D 1;L 5;R 2")]
+    [Example(answer: 36, "R 5;U 8;L 8;D 3;R 17;D 10;L 25;U 20")]
     [Puzzle(answer: 2619)]
     public long part_two(string input) => Simulate(input, 10);
     
@@ -21,9 +21,9 @@ public class Day_09
         {
             rope[0] += move;
 
-            for (var i = 0; i < size - 1; i++)
+            for (var i = 1; i < size; i++)
             {
-                rope[i + 1] = Tail(rope[i], rope[i + 1]);
+                rope[i] = Tail(rope[i - 1], rope[i]);
             }
             visited.Add(rope[^1]);
         }
@@ -33,14 +33,7 @@ public class Day_09
     static Vector Move(char c) => c switch { 'U' => Vector.N, 'D' => Vector.S, 'L' => Vector.W, _ /* R */ => Vector.E };
 
     static Point Tail(Point head, Point tail)
-    {
-        if (head - tail is { Length2: > 2 } delta)
-        {
-            var moved = head;
-            if (delta.X.Abs() > 1) moved += Vector.W * delta.X.Sign();
-            if (delta.Y.Abs() > 1) moved += Vector.N * delta.Y.Sign();
-            return moved;
-        }
-        else return tail;
-    }
+        => head - tail is { Length2: > 2 } delta
+        ? head - new Vector(delta.X / 2, delta.Y / 2)
+        : tail;
 }

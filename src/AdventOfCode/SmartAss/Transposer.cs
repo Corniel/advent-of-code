f9ae9f2daf6264d8859d03678de76b529738f908
Corniel Nobel <mail@corniel.nl>
2022-12-10 09:59:30 +0100

    Introduction of the extension .Transpose()

---
+++ src/AdventOfCode/SmartAss/Transposer.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartAss;
+
+internal static class Transposer
+{
+    public static IEnumerable<TResult> Transpose<TSource, TResult>(this TResult initial, IEnumerable<TSource> sources, Func<TResult, TSource, TResult> selector)
+    {
+        var next = initial;
+        yield return next;
+        foreach (var source in sources)
+        {
+            next = selector(next, source);
+            yield return next;
+        }
+    }
+}

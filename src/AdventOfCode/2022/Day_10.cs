58e9308915ba5ceb0e9b4e9ecce47a9059565239
Corniel Nobel <mail@corniel.nl>
2022-12-10 07:49:20 +0100

    2022-10

---
+++ src/AdventOfCode/2022/Day_10.cs
@@ -0,0 +1,39 @@
+﻿namespace Advent_of_Code_2022;
+
+[Category(Category.μs, Category.ASCII)]
+public class Day_10
+{
+    [Example(answer: 13140, 1)]
+    [Puzzle(answer: 12980)]
+    public int part_one(string input) => Data.Parse(input).Skip(19).WithStep(40).Select(x => x.Product).Sum();
+
+    [Puzzle(answer: "BRJLFULP")]
+    public string part_two(string input)
+    {
+        var grid = new Grid<bool>(40, 6);
+        grid.Set(true, Data.Parse(input).Where(d => d.Draw).Select(s => s.Point));
+        return grid.AsciiText();
+    }
+
+    record Data(int Cycle, int Strength)
+    {
+        public int Product => Cycle * Strength;
+        public Point Point => new((Cycle - 1).Mod(40), Cycle / 40);
+        public bool Draw => (Point.X - Strength).Abs() <= 1;
+
+        public static IEnumerable<Data> Parse(string input)
+        {
+            var strength = 1; var cycle = 1;
+            foreach (var line in input.Lines())
+            {
+                if (line == "noop") yield return new(cycle++, strength);
+                else
+                {
+                    yield return new(cycle++, strength);
+                    yield return new(cycle++, strength);
+                    strength += line[5..].Int32();
+                }
+            }
+        }
+    }
+}

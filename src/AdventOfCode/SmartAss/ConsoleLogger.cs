03f438eea1cd39b6ea2ff0b9bf51f64a1ce41661
Corniel Nobel <mail@corniel.nl>
2022-12-11 11:36:21 +0100

    Introduction of ConsoleLogger helpers.

---
+++ src/AdventOfCode/SmartAss/ConsoleLogger.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace SmartAss;
+
+/// <summary>Simplifies the usage of <see cref="Console.WriteLine"/>.</summary>
+/// <remarks>
+/// All run with <see cref="CultureInfo.InvariantCulture"/>.
+/// </remarks>
+public static class ConsoleLogger
+{
+    public static Grid<bool> ToConsole(this Grid<bool> grid)
+    {
+        WriteLine(grid?.ToString(c => c ? '█' : '░'));
+        return grid;
+    }
+
+    public static Grid<char> ToConsole(this Grid<char> grid)
+    {
+        WriteLine(grid?.ToString(c => c));
+        return grid;
+    }
+
+    public static Grid<int> ToConsole(this Grid<int> grid)
+    {
+        WriteLine(grid?.ToString(c => c.ToString()[0]));
+        return grid;
+    }
+
+    public static T ToConsole<T>(this T obj)
+    {
+        WriteLine(obj?.ToString());
+        return obj;
+    }
+
+    static void WriteLine(object obj) 
+    {
+        using var _ = CultureInfo.InvariantCulture.Scoped();
+        Console.WriteLine(obj ?? "{null}");
+    }
+}

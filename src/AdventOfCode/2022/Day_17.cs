1dd74652a4532b115b733b498e37233cd850456b
Corniel Nobel <mail@corniel.nl>
2022-12-17 22:22:03 +0100

    2022-17

---
+++ src/AdventOfCode/2022/Day_17.cs
@@ -0,0 +1,111 @@
+﻿namespace Advent_of_Code_2022;
+
+[Category(Category.Grid, Category.VectorAlgebra)]
+public class Day_17
+{
+    [Example(answer: 3068, ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")]
+    [Puzzle(answer: 3168, O.μs100)]
+    public long part_one(string input) => Play(input.Lines()[0], 2022);
+
+    [Example(answer: 1514285714288, ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")]
+    [Puzzle(answer: 1554117647070, O.μs100)]
+    public long part_two(string input) => Play(input.Lines()[0], 1_000_000_000_000);
+
+    static long Play(string shifts, long turns)
+    {
+        long extra = -1; long shift = 0;
+        var hashes = new Dictionary<Last14, Snapshot>();
+        var wall = new List<Row>() { new Row(255) };
+
+        for (long turn = 0; turn < turns; turn++)
+        {
+            var block = Block.New(turn, shifts, ref shift);
+            var offset = wall.Count;
+
+            while (!block.Overlaps(wall, offset - 1))
+            {
+                offset--;
+                var shifted = block.Shift(shifts, ref shift);
+                block = shifted.Overlaps(wall, offset) ? block : shifted;
+            }
+            foreach (var row in block.Rows)
+            {
+                if (offset < wall.Count) wall[offset] = wall[offset].Merge(row);
+                else wall.Add(row);
+                offset++;
+            }
+            if (turn.Mod(5) == 0)
+            {
+                var hash = Last14.New(wall);
+                
+                if (!hashes.TryAdd(hash, new(turn, wall.Count)))
+                {
+                    var first = hashes[hash];
+                    var period = turn - first.Turn;
+                    var periods = (turns - first.Turn) / period;
+                    var remainder = turns - periods * period - first.Turn;
+                    turns = turn + remainder;
+                    extra += (wall.Count - first.Count) * (periods - 1);
+                }
+            }
+        }
+        return wall.Count + extra;
+    }
+
+    record Snapshot(long Turn, int Count);
+
+    record struct Last14(long Lo, long Hi)
+    {
+        public static Last14 New(List<Row> rows)
+        {
+            long lo = 0; long hi = 0;
+            foreach (var row in rows.Skip(rows.Count - 7)) lo = lo << 7 | row.Index;
+            foreach (var row in rows.Skip(rows.Count - 14).Take(7)) hi = hi << 7 | row.Index;
+            return new(lo, hi);
+        }
+    }
+    
+    record struct Block(params Row[] Rows)
+    {
+        public static Block New(long turn, string shifts, ref long shift)
+        {
+            var block = Init[turn % 5];
+            for (var i = 0; i < 4; i++) block = block.Shift(shifts, ref shift);
+            return block;
+        }
+
+        public bool Overlaps(IReadOnlyList<Row> other, int offset)
+        {
+            for (var i = 0; i < Rows.Length; i++)
+            {
+                var o = i + offset;
+                if (o >= other.Count) return false;
+                if (Rows[i].Overlaps(other[o])) return true;
+            }
+            return false;
+        }
+
+        public Block Shift(string shifts, ref long shift) => shifts[(int)(shift++ % shifts.Length)] == '<' ? Left() : Right();
+        Block Left() => Rows.Select(r => r.Left()).ToArray() is { } rows && rows.All(r => r != default) ? new(rows) : this;
+        Block Right() => Rows.Select(r => r.Right()).ToArray() is { } rows && rows.All(r => r != default) ? new(rows) : this;
+
+        readonly static Block[] Init = new Block[]
+        {
+            /* - */ new(new Row(0b_00_111_10)),
+            /* + */ new(new Row(0b_00_010_00), new Row(0b_00_111_00), new Row(0b_00_010_00)),
+            /* J */ new(new Row(0b_00_111_00), new Row(0b_00_001_00), new Row(0b_00_001_00)),
+            /* | */ new(new Row(0b_00_100_00), new Row(0b_00_100_00), new Row(0b_00_100_00), new Row(0b_00_100_00)),
+            /* O */ new(new Row(0b_00_110_00), new Row(0b_00_110_00)),
+        };
+    }
+
+    record struct Row(byte Cells)
+    {
+        public long Index => Cells;
+        public bool Overlaps(Row other) => (Cells & other.Cells) != 0;
+        public Row Merge(Row other) => new((byte)(Cells | other.Cells));
+        public Row Left() => (Cells << 1) is var r && r < 128 ? new((byte)r) : default;
+        public Row Right() => (Cells >> 1) is var l && (l << 1) == Cells ? new((byte)l) : default;
+        public override string ToString() => Bits.Byte.ToString(Cells)[1..];
+    }
+}

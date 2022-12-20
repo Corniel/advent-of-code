446c72c900f8798e1022a4f4476b5b7c7691bf61
Corniel Nobel <mail@corniel.nl>
2022-12-20 21:39:58 +0100

    Loop, not circle.

--- src/AdventOfCode/2022/Day_20.cs
+++ src/AdventOfCode/2022/Day_20.cs
@@ -13,49 +13,12 @@ public class Day_20
 
     private static long Decrypt(string input, int key, int times)
     {
-        var ns = Number.Parse(input, key);
+        var ns = Loop.New(input.Int64s().Select(n => n * key));
+        var sort = ns.ToArray();
         var zero = ns.First(n => n.Value == 0);
 
-        foreach (var number in Repeat(ns, times).SelectMany(n => n)) number.Move();
+        foreach (var nr in Repeat(sort, times).SelectMany(n => n)) nr.Move(nr.Value);
 
-        return zero.Move(1000).Value + zero.Move(2000).Value + zero.Move(3000).Value;
-    }
-
-    public record Number(long Value)
-    {
-        public Number Prev { get; set; }
-        public Number Next { get; set; }
-        public int Shift { get; private set; }
-
-        public Number Move(int times)
-        {
-            var next = this;
-            while (times-- > 0) next = next.Next;
-            return next;
-        }
-
-        public void Move()
-        {
-            if (Shift == 0) return;
-
-            var prev = Prev; var next = Next;
-            prev.Next = next; next.Prev = prev;
-
-            var self = Move(Shift); prev = self.Next;
-            prev.Prev = this; self.Next = this;
-            this.Next = prev; this.Prev = self;
-        }
-
-        public static Number[] Parse(string input, long key)
-        {
-            var ns = input.Lines().Int32s().Select(n => new Number(n * key)).ToArray();
-            for (var i = 0; i < ns.Length; i++)
-            {
-                ns[i].Prev = ns[(i - 1).Mod(ns.Length)];
-                ns[i].Next = ns[(i + 1).Mod(ns.Length)];
-                ns[i].Shift = (int)ns[i].Value.Mod(ns.Length - 1);
-            }
-            return ns;
-        }
+        return zero.Skip(1000).Value + zero.Skip(2000).Value + zero.Skip(3000).Value;
     }
 }

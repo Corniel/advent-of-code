4f1ed2f3d2f24e138ca9c71ff8581e79781b4ded
Corniel Nobel <mail@corniel.nl>
2022-12-21 20:57:37 +0100

    Update 2015-07 to use Expr.

--- src/AdventOfCode/SmartAss/Expressions/Params.cs
+++ src/AdventOfCode/SmartAss/Expressions/Params.cs
@@ -5,6 +5,8 @@
 public sealed class Params : IReadOnlyCollection<Param>
 {
     private readonly Dictionary<string, Expr> Lookup = new();
+    private readonly Dictionary<string, long> Cache = new();
+    private bool WithCache;
 
     public int Count => Lookup.Count;
 
@@ -14,9 +16,32 @@ public sealed class Params : IReadOnlyCollection<Param>
         set => Lookup[name] = value;
     }
 
-    public long Value(string param) => TryValue(param) ?? throw new NotSolved();
-    
-    public long? TryValue(string param) => Lookup[param].TryValue(this);
+    public long Value(string param)
+    {
+        WithCache = true;
+        var value = TryValue(param) ?? throw new NotSolved();
+        WithCache = false;
+        Cache.Clear();
+        return value;
+    }
+
+    public long? TryValue(string param)
+    {
+        if (WithCache)
+        {
+            if (!Cache.TryGetValue(param, out var value))
+            {
+                if (Lookup[param].TryValue(this) is { } check)
+                {
+                    Cache[param] = check;
+                    return check;
+                }
+                else return null;
+            }
+            else return value;  
+        }
+        else return Lookup[param].TryValue(this);
+    }
 
     public IEnumerable<long> Values() => Lookup.Values.Select(v => v.Value(this));
 

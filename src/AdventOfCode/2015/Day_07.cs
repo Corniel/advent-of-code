295b627a0d76e07d8a8ab0034106667867077c9d
Corniel Nobel <mail@corniel.nl>
2022-12-21 20:57:37 +0100

    Update 2015-07 to use Expr.

--- src/AdventOfCode/2015/Day_07.cs
+++ src/AdventOfCode/2015/Day_07.cs
@@ -1,96 +1,40 @@
-﻿using Circuit = SmartAss.Circuits.Circuit<ushort>;
-using Node = SmartAss.Circuits.CircuitNode<ushort>;
+﻿namespace Advent_of_Code_2015;
 
-namespace Advent_of_Code_2015;
-
-[Category(Category.ExpressionParsing)]
+[Category(Category.ExpressionEvaluation)]
 public class Day_07
 {
     [Example(answer: 65079, "123 -> x;456 -> y;x AND y -> d;x OR y -> e;x LSHIFT 2 -> f;y RSHIFT 2 -> g;NOT x -> h;NOT y -> a")]
     [Puzzle(answer: 956, O.μs100)]
-    public ushort part_one(string input) => Parse(input)["a"].Output.Value;
+    public long part_one(string input) => Pars(input).Value("a");
 
     [Puzzle(answer: 40149, O.μs100)]
-    public ushort part_two(string input)
+    public long part_two(string input)
     {
-        var circuit = Parse(input);
-        circuit["b"] = new Circuit.Constant(956);
-        return circuit["a"].Output.Value;
+        var pars = Pars(input);
+        pars["b"] = SmartAss.Expressions.Expr.Const(956);
+        return pars.Value("a");
     }
 
-    private static Circuit Parse(string input)
-    {
-        var circuit = new Circuit();
+    public static Params Pars(string input) => Params.New(input.Lines(Param));
 
-        foreach (var line in input.Lines())
+    static Param Param(string line)
+    {
+        var parts = line.Separate(" ", "->");
+        return new(parts[^1], new Ushort(Expr(parts)));
+    }
+
+    static Expr Expr(IReadOnlyList<string> parts) => parts.Count switch
+    {
+        2 => Arg(parts[0]),
+        3 => new BitNot(Arg(parts[1])),
+        _ => parts[1] switch
         {
-            var split = line.Split(" -> ");
-            var instruction = split[0].Split(' ');
-            var assigned = split[1];
-
-            if (instruction.Length == 1 && ushort.TryParse(instruction[0], out var constant))
-            {
-                circuit[assigned] = new Circuit.Constant(constant);
-            }
-            else if (instruction.Length == 1)
-            {
-                circuit[assigned] = new Circuit.Assignment(circuit.NewVariable(instruction[0]));
-            }
-            else if (instruction.Length == 2 && instruction[0] == "NOT")
-            {
-                circuit[assigned] = new Not(New(instruction[1], circuit));
-            }
-            else if (instruction.Length == 3 && instruction[1] == "LSHIFT" && ushort.TryParse(instruction[2], out var lshift))
-            {
-                circuit[assigned] = new LShift(New(instruction[0], circuit), lshift);
-            }
-            else if (instruction.Length == 3 && instruction[1] == "RSHIFT" && ushort.TryParse(instruction[2], out var rshift))
-            {
-                circuit[assigned] = new RShift(New(instruction[0], circuit), rshift);
-            }
-            else if (instruction.Length == 3 && instruction[1] == "OR")
-            {
-                circuit[assigned] = new Or(New(instruction[0], circuit), New(instruction[2], circuit));
-            }
-            else if (instruction.Length == 3 && instruction[1] == "AND")
-            {
-                circuit[assigned] = new And(New(instruction[0], circuit), New(instruction[2], circuit));
-            }
-            else throw new FormatException($"'{line}' could not be parsed.");
+            "LSHIFT" => new ShiftLeft(Arg(parts[0]), Arg(parts[2])),
+            "RSHIFT" => new ShiftRight(Arg(parts[0]), Arg(parts[2])),
+            "AND" => new BitAnd(Arg(parts[0]), Arg(parts[2])),
+            "OR" or _ => new BitOr(Arg(parts[0]), Arg(parts[2])),
         }
+    };
 
-        return circuit;
-
-        static Node New(string str, Circuit circuit)
-            => ushort.TryParse(str, out var constant)
-            ? new Circuit.Constant(constant)
-            : circuit.NewVariable(str);
-    }
-
-    record Not(Node Other) : Node
-    {
-        protected override ushort? Execute() => Other.Output.HasValue ? (ushort)~Other.Output.Value : null;
-    }
-    record And(Node Left, Node Right) : Node
-    {
-        protected override ushort? Execute()
-            => Left.Output.HasValue && Right.Output.HasValue
-            ? (ushort)(Left.Output.Value & Right.Output.Value)
-            : null;
-    }
-    record Or(Node Left, Node Right) : Node
-    {
-        protected override ushort? Execute() 
-            => Left.Output.HasValue  && Right.Output.HasValue
-            ? (ushort)(Left.Output.Value | Right.Output.Value)
-            : null;
-    }
-    record LShift(Node Other, int Shift): Node
-    {
-        protected override ushort? Execute() => Other.Output.HasValue ? (ushort)(Other.Output.Value << Shift) : null;
-    }
-    record RShift(Node Other, int Shift) : Node
-    {
-        protected override ushort? Execute() => Other.Output.HasValue ? (ushort)(Other.Output.Value >> Shift) : null;
-    }
+    static Expr Arg(string str) => str.Int32N() is { } n ? SmartAss.Expressions.Expr.Const(n) : SmartAss.Expressions.Expr.Ref(str);
 }

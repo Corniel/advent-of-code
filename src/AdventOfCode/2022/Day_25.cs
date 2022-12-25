32aa9a9a1598f333f2b2ee32fc893bd99a5d944b
Corniel Nobel <mail@corniel.nl>
2022-12-25 21:51:16 +0100

    2022-25

---
+++ src/AdventOfCode/2022/Day_25.cs
@@ -0,0 +1,31 @@
+﻿namespace Advent_of_Code_2022;
+
+[Category(Category.ExpressionParsing)]
+public class Day_25
+{
+    [Example(answer: "1=-0-2", "1=-0-2")]
+    [Puzzle(answer: "2=222-2---22=1=--1-2", O.μs10)]
+    public string part_one(string input) => SNAFU(input.Lines(Number).Sum());
+
+    [Puzzle(answer: "You only need 49 stars to boost it", "You only need 49 stars to boost it")]
+    public string part_two(string input) => input;
+
+    private static long Number(string input)
+    {
+        long n = 0;
+        foreach (var ch in input) n = n * 5 + "=-012".IndexOf(ch) - 2;
+        return n;
+    }
+
+    public static string SNAFU(long n)
+    {
+        var sb = new StringBuilder();
+        while (n > 0)
+        {
+            n += 2;
+            sb.Insert(0, "=-012"[(int)(n % 5)]);
+            n /= 5;
+        }
+        return sb.ToString();
+    }
+}

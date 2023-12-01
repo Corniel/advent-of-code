441e868c386ccd84eac84904fa6494a3548507d7
Corniel Nobel <corniel@gmail.com>
2023-12-01 11:48:18 +0100

    2023-01

---
+++ src/AdventOfCode/2023/Day_01.cs
@@ -0,0 +1,32 @@
+﻿namespace Advent_of_Code_2023;
+
+[Category(Category.Cryptography)]
+public class Day_01
+{
+    [Example(answer: 142, "1abc2;pqr3stu8vwx;a1b2c3d4e5f;treb7uchet")]
+    [Puzzle(answer: 56108, O.μs100)]
+    public int part_one(string input) => input.Lines(l => Number(l, false)).Sum();
+
+    [Example(answer: 281, "two1nine;eightwothree;abcone2threexyz;xtwone3four;4nineeightseven2;zoneight234;7pqrstsixteen")]
+    [Example(answer: 83, "eightwothree")]
+    [Example(answer: 88, "vrpplrtqxvssgnvdf8")]
+    [Example(answer: 54925, Example._1)]
+    [Puzzle(answer: 55652, O.ms)]
+    public int part_two(string input) => input.Lines(l => Number(l, true)).Sum();
+
+    int Number(string line, bool words)
+    {
+        var n = line.Select(Digit).WithValue().ToArray();
+        return n[0] * 10 + n[^1];
+
+        int? Digit(char ch, int i)
+        {
+            if (char.IsDigit(ch)) return ch - '0';
+            else return words && line[i..] is { Length: >= 3 } ln
+                ? Range(1, 9).FirstOrNone(n => ln.StartsWith(ws[n]))
+                : null;
+        }
+    }
+
+    readonly string[] ws = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+}

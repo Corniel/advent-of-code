71bc7f0ba62295715b8c0f6811bec6c17349a4ea
Corniel Nobel <corniel@gmail.com>
2023-12-02 07:03:16 +0100

    Fix stricter line splitter.

--- src/AdventOfCode/2022/Day_03.cs
+++ src/AdventOfCode/2022/Day_03.cs
@@ -3,11 +3,11 @@
 [Category(Category.Cryptography)]
 public class Day_03
 {
-    [Example(answer: 157, "vJrwpWtwJgWrhcsFMMfFFhFp;jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL;PmmdzqPrVvPwwTWBwg;wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn;ttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw")]
+    [Example(answer: 157, "vJrwpWtwJgWrhcsFMMfFFhFp;jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL;PmmdzqPrVvPwwTWBwg;wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn;ttgJtRGJQctTZtZT;CrZsJsPPZsGzwwsLwLmpwMDw")]
     [Puzzle(answer: 7742, O.μs10)]
     public int part_one(Lines input) => input.Select(Shared).Sum();
 
-    [Example(answer: 70, "vJrwpWtwJgWrhcsFMMfFFhFp;jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL;PmmdzqPrVvPwwTWBwg;wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn;ttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw")]
+    [Example(answer: 70, "vJrwpWtwJgWrhcsFMMfFFhFp;jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL;PmmdzqPrVvPwwTWBwg;wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn;ttgJtRGJQctTZtZT;CrZsJsPPZsGzwwsLwLmpwMDw")]
     [Puzzle(answer: 2276, O.μs10)]
     public int part_two(Lines input) => input.ChunkBy(3).Select(Shared).Sum();
 

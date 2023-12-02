945fd66e8bdc382d7e87007653a4fe7a781a230e
Corniel Nobel <corniel@gmail.com>
2023-12-02 07:01:06 +0100

    2023-02

---
+++ src/AdventOfCode/2023/Day_02.cs
@@ -0,0 +1,41 @@
+﻿namespace Advent_of_Code_2023;
+
+[Category(Category.ExpressionParsing)]
+public class Day_02
+{
+    readonly Hand fit = new(12, 13, 14);
+
+    [Example(answer: 8, Example._1)]
+    [Puzzle(answer: 2551, O.μs100)]
+    public int part_one(Lines input)
+        => input.Select(Game.parse).Where(g => g.Hands.TrueForAll(h => h.Fits(fit))).Sum(g => g.Id);
+    
+    [Example(answer: 2286, Example._1)]
+    [Puzzle(answer: 62811, O.μs100)]
+    public int part_two(Lines input) => input.Select(Game.parse).Select(g => g.Smallest.Pow).Sum();
+
+    record Game(int Id, Hand[] Hands)
+    {
+        public Hand Smallest => new(Hands.Max(h => h.R), Hands.Max(h => h.G), Hands.Max(h => h.B));
+
+        public static Game parse(string line) => new(
+            line.Int32(),
+            line[line.IndexOf(':')..].Separate(';').Select(Hand.Parse).ToArray());
+    }
+
+    record struct Hand(int R, int G, int B)
+    {
+        public int Pow => R * G * B;
+
+        public bool Fits(Hand other) => other.R >= R && other.G >= G && other.B >= B;
+
+        public static Hand Parse(string str)
+        {
+            var parts = str.Separate(',').ToArray();
+            return new(
+                parts.Find(p => p.EndsWith("red"))?.Int32() ?? 0,
+                parts.Find(p => p.EndsWith("green"))?.Int32() ?? 0,
+                parts.Find(p => p.EndsWith("blue"))?.Int32() ?? 0);
+        }
+    }
+}
--- src/AdventOfCode/Extensions/System.Array.cs
+++ src/AdventOfCode/Extensions/System.Array.cs
@@ -4,6 +4,8 @@ public static class ArrayExtensions
 {
     public static bool Exists<T>(this T[] array, Predicate<T> predicate) => Array.Exists(array, predicate);
 
+    public static T? Find<T>(this T[] array, Predicate<T> predicate) => Array.Find(array, predicate);
+
     public static bool TrueForAll<T>(this T[] array, Predicate<T> predicate) => Array.TrueForAll(array, predicate);
 
     public static int IndexOf<T>(this T[] array, T value) => Array.IndexOf(array, value);

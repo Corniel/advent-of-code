60c4a0efa9b94a9931d74abd60ba5f82e129737f
Corniel Nobel <corniel@gmail.com>
2023-12-05 11:58:42 +0100

    Simplify 2018-10.

--- src/AdventOfCode/2018/Day_10.cs
+++ src/AdventOfCode/2018/Day_10.cs
@@ -22,42 +22,33 @@ public class Day_10
     static (int Seconds, Grid<bool> Grid) Simulate(Lines lines)
     {
         var dots = lines.As(Dot.Parse).ToArray();
-        var seconds = 0;
-        while (true)
+        for (var s = 1; s < int.MaxValue; s++)
         {
-            seconds++;
+            foreach (var dot in dots) dot.Position += dot.Velocity;
 
-            foreach (var dot in dots)
-            {
-                dot.Position += dot.Velocity;
-            }
-
-            var min_x = dots.Min(d => d.Position.X);
             var min_y = dots.Min(d => d.Position.Y);
-            var max_x = dots.Max(d => d.Position.X);
             var max_y = dots.Max(d => d.Position.Y);
 
             if (max_y - min_y == 9)
             {
-                var min = new Vector(min_x, min_y);
-                var max = new Vector(max_x, max_y);
+                var min = new Vector(dots.Min(d => d.Position.X), min_y);
+                var max = new Vector(dots.Max(d => d.Position.X), max_y);
                 var grid = new Grid<bool>(cols: (max - min).X + 1, rows: (max - min).Y + 1);
                 grid.Set(true, dots.Select(dot => dot.Position - min));
-                return (seconds, grid);
+                return (s, grid);
             }
         }
         throw new InfiniteLoop();
     }
 
-    class Dot
+    record class Dot(Vector Velocity)
     {
         public Point Position { get; set; }
-        public Vector Velocity { get; init; }
 
         public static Dot Parse(string line)
         {
             var numbers = line.Int32s().ToArray();
-            return new() { Position = new(numbers[0], numbers[1]), Velocity = new(numbers[2], numbers[3]) };
+            return new(new Vector(numbers[2], numbers[3])) { Position = new(numbers[0], numbers[1]) };
         }
     }
 }

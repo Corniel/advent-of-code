6bbf8e0f8346ebc6e036e03886fab715050c6673
Corniel Nobel <corniel@gmail.com>
2023-12-08 13:46:30 +0100

    Add personal TODO list (simple puzzles first).

--- src/AdventOfCode.Utils/Rankings/Participants.cs
+++ src/AdventOfCode.Utils/Rankings/Participants.cs
@@ -4,4 +4,7 @@ public class Participants : Dictionary<long, Participant>
 {
     public Participants() { }
     public Participants(IEnumerable<KeyValuePair<long, Participant>> collection) : base(collection) { }
+
+    public Participant? Search(string name)
+        => Values.FirstOrDefault(p => p.Matches(name));
 }

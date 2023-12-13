11171c0b7081013d3551585b9feae586a4bf5211
Corniel Nobel <corniel@gmail.com>
2023-12-13 18:13:38 +0100

    Rewrote Day 12 based on @Renzo Baasdam's implementation.

--- src/AdventOfCode/2023/Day_12.cs
+++ src/AdventOfCode/2023/Day_12.cs
@@ -1,119 +1,120 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Advent_of_Code_2023;
+﻿namespace Advent_of_Code_2023;
 
 [Category(Category.Permutations)]
 public class Day_12
 {
     [Example(answer: 7, "?.???? 1,1")]
     [Example(answer: 1, "??#.# 2,1")]
-    [Example(answer: 1, "#.# 1,1")]
-    [Example(answer: 3, "???? 1,1")]
-    [Example(answer: 1, "??? 1,1")]
-    [Example(answer: 4, "???? 1")]
-    [Example(answer: 3, "???? 2")]
     [Example(answer: 1, "???.### 1,1,3")]
     [Example(answer: 2, "????.#...#... 3,1,1")]
-    [Example(answer: 1, "????.#...#... 4,1,1")]
     [Example(answer: 8, "#??.???..? 1,1,1")]
     [Example(answer: 4, "?#?.?#? 2,2")]
-    [Example(answer: 10, "??????? 2,1")]
     [Example(answer: 11899700525790, "??????????????????????????????????????????????????????????????????????????????? 1,3,1,1,3,1,1,3,1,1,3,1,1,3,1")]
     [Example(answer: 10, "?###???????? 3,2,1")]
     [Example(answer: 4, "????.######..#####. 1,6,5")]
-    [Example(answer: 1, ".#?????..???????.? 6,7")]
     [Puzzle(answer: 7541L, O.ms)]
-    public long part_one(Lines lines) => lines.As(l => Block.Parse(l, 1)).Sum(b => b.Permutations);
+    public long part_one(Lines lines) => lines.As(l => Condition.Parse(l, 1)).Sum(c => c.Arrangements());
 
     [Example(answer: 1, "???.### 1,1,3")]
     [Example(answer: 16, "????.#...#... 4,1,1")]
     [Example(answer: 16384, ".??..??...?##. 1,1,3")]
     [Example(answer: 506250, "?###???????? 3,2,1")]
-    [Puzzle(answer: 17485169859432, O.s100)]
-    public long part_two(Lines lines) => lines.As(l => Block.Parse(l, 5)).Sum(b => b.Permutations);
+    [Puzzle(answer: 17485169859432, O.ms10)]
+    public long part_two(Lines lines) => lines.As(l => Condition.Parse(l, 5)).Sum(c => c.Arrangements());
 
-    record Block(string[] Data, int[] Blocks)
+    record Condition(string Pattern, int[] Frequencies)
     {
-        readonly Dictionary<Hash, long> Cache = [];
-
-        public long Permutations => Next(Data, Blocks);
-
-        long Next(string[] datas, int[] blocks)
-        {
-            if (datas.Sum(d => d.Length) < blocks.Sum()) return 0;
-
-            var data = datas[0];
-
-            if (datas.Length == 1) return Next(data, blocks);
-
-            var count = 0L;
-
-            for (var l = 0; l <= blocks.Length; l++)
-            {
-                var n = Next(data, blocks[..l]);
-                if (n > 0) count += n * Next(datas[1..], blocks[l..]);
-            }
-            return count;
-        }
-
-        long Next(string s, int[] blocks)
-        {
-            var hash = new Hash(s, blocks);
-
-            if (Cache.TryGetValue(hash, out var n)) return n;
-
-            if (blocks.Length == 0)
-            {
-                return (s.Length == 0 || s.All(c => c == '?')) ? 1 : 0;
-            }
-            if (s.Length == 0) return 0;
-
-            var len = blocks[0];
-
-            // One fit.
-            if (blocks.Length == 1 && s.Length == len) return 1;
-
-            var min = blocks.Sum() + blocks.Length - 1;
-            // No fit, not enough chars left.
-            if (s.Length < min) return 0;
-
-            return Cache[hash] = s.All(c => c == '?') 
-                ? Blanks(s, blocks)
-                : NoneBlank(s, blocks, len, min);
-        }
-
-        long NoneBlank(string s, int[] blocks, int len, int min) => 0L
-            // Assume '.'
-            + (s[0] == '?' && s.Length > min ? Next(s[1..], blocks) : 0)
-            // Split due as s[len] assumed '.'
-            + (s[len] != '#'? Next(s[(len + 1)..], blocks[1..]) : 0);
-
-        static long Blanks(string s, int[] blocks)
-        {
-            var stars = s.Length - blocks.Sum();
-
-            if (stars + 1 >= blocks.Length)
-            {
-                return Maths.Combination(stars + 1, blocks.Length);
-            }
-            var bars = blocks.LongLength + 1;
-
-            return Maths.Combination(bars, stars);
-        }
-
-        public static Block Parse(string line, int rep = 1) => new(
-            string.Join('?', Repeat(line.Split(' ')[0], rep)).Separate('.'), 
+        public static Condition Parse(string line, int rep) => new(
+            string.Join('?', Repeat(Trim(line), rep)),
             Repeat(line.Int32s(), rep).SelectMany(n => n).ToArray());
+
+        // This replacement is a modification of @Renzo Baasdam's. Mine was
+        // correct, but did not scale, the part two variant took 2 minutes
+        // instead of the current 10 ms.
+        public long Arrangements()
+        {
+            if (Pattern.All(c => c == '?')) return Blanks();
+
+            var curr = new States();
+            var next = new States();
+
+            // Start with on arrangement.
+            curr.Init(Frequencies);
+            curr[0].Add(idx: 0, 1);
+
+            foreach (var ch in Pattern)
+            {
+                next.Init(Frequencies);
+
+                switch (ch)
+                {
+                    case '.': curr.Dot(next); break;
+                    case '#': curr.Hash(next);break;
+                    default: curr.Dot(next); curr.Hash(next); break; // ?
+                }
+                (curr, next) = (next, curr);
+            }
+            return curr[^2..].Sum(s => s.Last);
+        }
+        
+        // This is part of my old implementation; it uses some combination theory
+        // and is even (just slightly) faster then the fall back)
+        long Blanks()
+        {
+            var stars = Pattern.Length - Frequencies.Sum();
+            return stars + 1 >= Frequencies.Length
+                ? Maths.Combination(stars + 1, Frequencies.Length)
+                : Maths.Combination(Frequencies.Length + 1L, stars);
+        }
+
+        static string Trim(string line) => line.Split(' ')[0].RegReplace("\\.{2,}", ".");
     }
 
-    readonly struct Hash(string pattern, int[] blocks) : IEquatable<Hash>
+    sealed class States : List<State>
     {
-        readonly string Pattern = pattern; readonly int[] Blocks = blocks;
+        public void Init(int[] frequencies)
+        {
+            Clear();
+            AddRange(frequencies.Select(f => new State(f + 1)));
+            Add(new(1));
+        }
 
-        public override bool Equals([NotNullWhen(true)] object obj) => obj is Hash hash && Equals(hash);
+        public void Dot(States next)
+        {
+            next[0].Add(idx: 0, this[0].First);
 
-        public bool Equals(Hash other) => Pattern == other.Pattern && Enumerable.SequenceEqual(Blocks, other.Blocks);
+            for (var st = 1; st < Count; st++)
+            {
+                next[st].Add(idx: 0, this[st].First + this[st - 1].Last);
+            }
+        }
 
-        public override int GetHashCode() => Qowaiv.Hashing.Hash.Code(pattern).And(blocks);
+        public void Hash(States next)
+        {
+            for (var st = 0; st < Count; st++)
+            {
+                for (var freq = 0; freq < this[st].Count - 1; freq++)
+                {
+                    next[st].Add(idx: freq + 1, this[st][freq]);
+                }
+            }
+        }
+    }
+
+    readonly struct State(int length)
+    {
+        private readonly long[] Counts = new long[length];
+
+        public long this[int index] => Counts[index];
+
+        public int Count => Counts.Length;
+
+        public void Add(int idx, long val) => Counts[idx] += val;
+
+        public long First => this[0];
+
+        public long Last => this[^1];
+
+        public override string ToString() => string.Join(", ", Counts);
     }
 }
---
+++ src/AdventOfCode/Extensions/System.String.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace System;
+
+public static class AoCStringExtensions
+{
+    /// <inheritdoc cref="Regex.Replace(string, string)" />
+    public static string RegReplace(this string str, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern, string replacement)
+        => Regex.Replace(str, pattern, replacement);
+}

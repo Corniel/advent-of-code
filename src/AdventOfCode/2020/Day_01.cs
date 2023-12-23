33dc6fc5aae89e77d1aef250630235cfbb2b7925
Corniel Nobel <corniel@gmail.com>
2023-12-23 12:48:27 +0100

    2023-23

--- src/AdventOfCode/2020/Day_01.cs
+++ src/AdventOfCode/2020/Day_01.cs
@@ -5,21 +5,21 @@ public class Day_01
 {
     [Example(answer: 514579, "1721, 979, 366, 299, 675, 1456")]
     [Puzzle(answer: 786811, O.μs)]
-    public int part_one(Ints input)
+    public int part_one(Ints numbers)
     {
-        var numbers = new UniqueNumbers(input);
-        var n = numbers.Range(max: 2020 / 2).First(n => numbers.Contains(2020 - n));
+        var ns = new UniqueNumbers(numbers);
+        var n = ns.Range(max: 2020 / 2).First(n => ns.Contains(2020 - n));
         return n * (2020 - n);
     }
 
     [Example(answer: 241861950L, "1721, 979, 366, 299, 675, 1456")]
     [Puzzle(answer: 199068980L, O.μs)]
-    public long part_two(Ints input)
+    public long part_two(Ints numbers)
     {
-        var numbers = new UniqueNumbers(input);
-        foreach (var n0 in numbers.Range(max: 2020 / 3))
+        var ns = new UniqueNumbers(numbers);
+        foreach (var n0 in ns.Range(max: 2020 / 3))
         {
-            if (numbers.Range(min: n0 + 1, max: (2020 - n0) / 2).FirstOrNone(n => numbers.Contains(2020 - n0 - n)) is { } n1)
+            if (ns.Range(min: n0 + 1, max: (2020 - n0) / 2).FirstOrNone(n => ns.Contains(2020 - n0 - n)) is { } n1)
                 return 1L * n0 * n1 * (2020 - n0 - n1);
         }
         throw new NoAnswer();

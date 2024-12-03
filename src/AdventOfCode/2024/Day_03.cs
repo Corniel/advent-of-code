bc9ae4c800b0f7279762c10f28e30e4c5b4b863f
Corniel Nobel <corniel@gmail.com>
2024-12-03 09:26:35 +0100

    Speed improvements.

--- src/AdventOfCode/2024/Day_03.cs
+++ src/AdventOfCode/2024/Day_03.cs
@@ -1,26 +1,29 @@
 namespace Advent_of_Code_2024;
 
 [Category(Category.ExpressionParsing)]
-public class Day_03
+public partial class Day_03
 {
     [Example(answer: 161, "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")]
-    [Puzzle(answer: 183788984, O.μs100)]
-    public int part_one(string str) => Parse(str, new(@"mul\(\d+,\d+\)"));
+    [Puzzle(answer: 183788984, O.μs10)]
+    public int part_one(string str) => Parse(str, true);
 
     [Example(answer: 48, "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")]
-    [Puzzle(answer: 62098619, O.μs100)]
-    public int part_two(string str) => Parse(str, new(@"(mul\(\d+,\d+\)|do\(\)|don't\(\))"));
+    [Puzzle(answer: 62098619, O.μs10)]
+    public int part_two(string str) => Parse(str, false);
 
-    static int Parse(string str, Regex pattern)
+    static int Parse(string str, bool dont)
     {
         var sum = 0; var enable = true;
 
-        foreach (Group m in pattern.Matches(str))
+        foreach (var m in Reg().Matches(str).Select(m => m.Value))
         {
-            if (m.Value == "don't()") enable = false;
-            else if (m.Value == "do()") enable = true;
-            else if (enable) sum += m.Value.Int32s().Product();
+            if (m == "don't()") enable = dont;
+            else if (m == "do()") enable = true;
+            else if (enable) sum += m.Int32s().Product();
         }
         return sum;
     }
+
+    [GeneratedRegex(@"(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))", RegexOptions.ExplicitCapture)]
+    private static partial Regex Reg();
 }

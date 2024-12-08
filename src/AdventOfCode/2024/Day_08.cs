4ceed23eeb7753f135013dafaf65f9f3e353f980
Corniel Nobel <corniel@gmail.com>
2024-12-08 14:46:56 +0100

    2024-08

---
+++ src/AdventOfCode/2024/Day_08.cs
@@ -0,0 +1,23 @@
+namespace Advent_of_Code_2024;
+
+[Category(Category.Grid)]
+public class Day_08
+{
+    [Example(answer: 14, Example._1)]
+    [Puzzle(answer: 278, O.μs10)]
+    public int part_one(CharGrid map) => Scan(map, One);
+
+    [Example(answer: 34, Example._1)]
+    [Puzzle(answer: 1067, O.μs10)]
+    public int part_two(CharGrid map) => Scan(map, Two);
+
+    private static int Scan(CharGrid map, Func<Point, Point, CharGrid, IEnumerable<Point>> add) => map
+        .Positions(p => p != '.').GroupBy(p => map[p])
+        .SelectMany(ps => ps.RoundRobin())
+        .SelectMany(ps => add(ps.First, ps.Second, map).Concat(add(ps.Second, ps.First, map)))
+        .Distinct().Count();
+
+    static IEnumerable<Point> One(Point p1, Point p2, CharGrid map) => p1.Repeat(p1 - p2).TakeWhile(map.OnGrid).Take(1);
+
+    static IEnumerable<Point> Two(Point p1, Point p2, CharGrid map) => p1.Repeat(p1 - p2, true).TakeWhile(map.OnGrid);
+}

35ec952e09e9cb10db30daeb705ea8c58e57485b
Corniel Nobel <corniel@gmail.com>
2024-12-11 09:54:47 +0100

    2024-11

---
+++ src/AdventOfCode/2024/Day_11.cs
@@ -0,0 +1,39 @@
+namespace Advent_of_Code_2024;
+
+[Category(Category.SequenceProgression)]
+public class Day_11
+{
+    [Puzzle(answer: 193269, "475449 2599064 213 0 2 65 5755 51149", O.μs100)]
+    public int part_one(Longs numbers) => (int)Blink(numbers, 25);
+
+    [Puzzle(answer: 228449040027793, "475449 2599064 213 0 2 65 5755 51149", O.ms)]
+    public long part_two(Longs numbers) => Blink(numbers, 75);
+
+    static long Blink(Longs numbers, int blinks)
+    {
+        var curr = ItemCounter.New(numbers); var next = ItemCounter.New<long>();
+
+        for (var blink = 0; blink < blinks; blink++)
+        {
+            foreach (var ic in curr)
+            {
+                var n = ic.Item; var count = ic.Count;
+
+                if (n == 0) next[1] += count;
+                else
+                {
+                    var digits = n.DigitCount();
+                    if (digits.IsEven())
+                    {
+                        var f = 10.Pow(digits / 2);
+                        next[n / f] += count; 
+                        next[n % f] += count;
+                    }
+                    else next[n * 2024] += count;
+                }
+            }
+            (curr, next) = (next, curr.Clear());
+        }
+        return curr.Total;
+    }
+}
